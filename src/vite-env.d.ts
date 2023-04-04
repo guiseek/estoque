597cf5ec7ad493ee961e137dff1eb09adaadb53a
Gui Seek <guiseek@gmail.com>
2023-04-03 22:05:08 -0300

    feat: interfaces

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="vite/client" />
+
+interface Product {
+  id: number
+  name: string
+  price: string
+  detail?: string
+  amount?: string
+  category?: string
+  visible?: boolean
+  created: Date
+  updated: Date
+}
+
+interface CreateProductDto {
+  name: string
+  price: string
+  detail?: string
+  amount?: string
+  category?: string
+  visible?: boolean
+}
+
+interface UpdateProductDro extends Partial<CreateProductDto> {
+  id: number
+}
